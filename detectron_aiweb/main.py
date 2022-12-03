39613ce6b19c04ce42fa8e117d486babab43e134
GitHub-OfficialAccount <chen.yichen.2021@vjc.edu.sg>
2022-12-03 19:59:17 +0800

    test

--- detectron_aiweb/main.py
+++ detectron_aiweb/main.py
@@ -2,10 +2,10 @@ from Detector import *
 
 detector = Detector()
 
+import os
 #get image path
-import cv2, os
-file_detectron2 = os.path.abspath('detectron2')
-file_images = os.path.join(file_detectron2, 'images')
-file_image = os.path.join(file_images, 'sheep.jpg')
+image = 'sheep.jpg'
+path_detectron2 = os.path.abspath('detectron_aiweb')
+path_image = os.path.join(path_detectron2,'images',image)
 
-detector.onImage(file_image)
+detector.onImage(path_image)
