dc2e3e95daf848fe11c1d5aa8ad2b75b8f3430ad
GitHub-OfficialAccount <chen.yichen.2021@vjc.edu.sg>
2022-12-04 15:17:46 +0800

    refined functions.py

--- aiweb/explorer/functions.py
+++ aiweb/explorer/functions.py
@@ -1,9 +1,10 @@
-import os,sys
+import os
 
 path = os.getcwd()
 parent_path = os.path.dirname(path)
 
 ### add detectron path ###
+import sys
 detectron_path = os.path.join(parent_path,'detectron_aiweb')
 assert os.path.exists(detectron_path)
 sys.path.append(detectron_path)
