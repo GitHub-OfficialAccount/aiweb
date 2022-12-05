8da087ae8c98bf245c2e6f250c087e55e68db05e
GitHub-OfficialAccount <chen.yichen.2021@vjc.edu.sg>
2022-12-05 22:03:49 +0800

    proven ability to self-train using customised datasets. Note: Model is of very low accuracy due to trash cpu; Only Object-detection is added. Instance Segmentation is yet to be added; self-trained BloodCell model is removed due to overly large size, but could be trained again easily

--- detectron_aiweb/mydatasets_train.py
+++ detectron_aiweb/mydatasets_train.py
@@ -13,25 +13,29 @@ from mydatasets_utils import *
 config_file_path = "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
 checkpoint_url = "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
 
-output_dir = "./output/object_detection"
-num_classes = 1
-
-device = "cpu" #or cuda
-
 # path = os.getcwd() ##for django
 path = os.path.abspath('detectron_aiweb')
+
+output_dir = os.path.join(path, "mydatasets_output/object_detection")
+
+num_classes = 3 #depends on how many types of object are being identified
+
+device = "cpu" #or cuda
 
 train_dataset_name = "RBC_train"
 train_images_path =  os.path.join(path,"mydatasets_train")
 train_json_annot_path = os.path.join(path,"mydatasets_train.json")
-assert os.path.exists(train_images_path)
-assert os.path.exists(train_json_annot_path)
 
 test_dataset_name = "RBC_test"
 test_images_path = os.path.join(path,"mydatasets_test")
 test_json_annot_path = os.path.join(path,"mydatasets_test.json")
+
+assert os.path.exists(train_images_path)
+assert os.path.exists(train_json_annot_path)
 assert os.path.exists(test_images_path)
 assert os.path.exists(test_json_annot_path)
+
+cfg_save_path = os.path.join(path,"OD_cfg.pickle")
 
 #############################
 register_coco_instances(
@@ -46,4 +50,35 @@ register_coco_instances(
     json_file=test_json_annot_path,
     image_root=test_images_path)
 
-plot_samples(dataset_name=train_dataset_name, n=2)
+# plot_samples(dataset_name=train_dataset_name, n=2)
+
+##########################
+
+def main():
+    cfg = get_train_cfg(config_file_path, checkpoint_url, train_dataset_name, test_dataset_name, num_classes, device, output_dir)
+
+    with open(cfg_save_path,'wb') as f: #'wb': write in binary mode
+        pickle.dump(cfg, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+
+    trainer = DefaultTrainer(cfg)
+    trainer.resume_or_load(resume=False)
+
+    trainer.train()
+
+if __name__ == '__main__':
+    main()
+
+### the smaller loss, the higher accuracy
+# 0.00: Perfect; <0.02: Great; <0.05: Good
+# total_loss: This is a weighted sum of the following individual losses calculated during the iteration. By default, the weights are all one.
+# loss_cls: Classification loss in the ROI head. Measures the loss for box classification, i.e., how good the model is at labelling a predicted box with the correct class.
+# loss_box_reg: Localisation loss in the ROI head. Measures the loss for box localisation (predicted location vs true location).
+# loss_rpn_cls: Classification loss in the Region Proposal Network. Measures the "objectness" loss, i.e., how good the RPN is at labelling the anchor boxes as foreground or background.
+# loss_rpn_loc: Localisation loss in the Region Proposal Network. Measures the loss for localisation of the predicted regions in the RPN.
+# loss_mask: Mask loss in the Mask head. Measures how "correct" the predicted binary masks are.
+# time: Time taken by the iteration.
+# data_time: Time taken by the dataloader in that iteration.
+# lr: The learning rate in that iteration.
+# max_mem: Maximum GPU memory occupied by tensors in bytes.
--- detectron_aiweb/mydatasets_utils.py
+++ detectron_aiweb/mydatasets_utils.py
@@ -19,3 +19,56 @@ def plot_samples(dataset_name, n=1):
         plt.figure(figsize=(15,20))
         plt.imshow(v.get_image())
         plt.show()
+
+def get_train_cfg(config_file_path, checkpoint_url, train_dataset_name, test_dataset_name, num_classes, device, output_dir):
+    cfg = get_cfg()
+
+    cfg.merge_from_file(model_zoo.get_config_file(config_file_path))
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(checkpoint_url)
+    cfg.DATASETS.TRAIN = (train_dataset_name,)
+    cfg.DATASETS.TEST = (test_dataset_name,)
+
+    ###the more workers the faster the data is loaded, but this stresses the cpu more###
+    cfg.DATALOADER.NUM_WORKERS = 2
+
+    ###change if cpu is slow (eg, reduce iteration) ###
+    cfg.SOLVER.IMS_PER_BATCH = 2
+    cfg.SOLVER.BASE_LR = 0.00025
+    cfg.SOLVER.MAX_ITER = 20
+    cfg.SOLVER.STEPS = []
+
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = num_classes
+    cfg.MODEL.DEVICE = device
+    
+    cfg.OUTPUT_DIR = output_dir
+
+    return cfg
+
+def on_image(image_path, predictor):
+    im = cv2.imread(image_path)
+    outputs = predictor(im)
+    v = Visualizer(im[:,:,::-1], metadata={}, scale=0.5, instance_mode=ColorMode.SEGMENTATION)
+    v = v.draw_instance_predictions(outputs["instances"].to('cpu'))
+
+    plt.figure(figsize=(14,10))
+    plt.imshow(v.get_image())
+    plt.show()
+
+def onvideo(videoPath, predictor):
+    cap = cv2.VideoCapture(videoPath)
+    if (cap.isOpened() == False):
+        print('Error opening file...')
+        return
+    
+    (success, image) = cap.read()
+    while success:
+        predictions = predictor(image)
+        v = Visualizer(image[:,:,::-1], metadata={}, instance_mode=ColorMode.SEGMENTATION)
+        output = v.draw_instance_predictions(predictions["instances"].to("cpu"))
+
+        cv2.imshow("Result", output.get_image()[:,:,::-1])
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+        (success, image) = cap.read()
